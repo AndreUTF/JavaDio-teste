e3369edcf13d3cbf11d9d6fbe59b22e14c79805e
André Portes Ferriani <andre.portes17@gmail.com>
2021-02-01 23:27:46 -0300

    main

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,6 +1,9 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Test Java");
+        System.out.println("Test Java");
+        System.out.println("Test Java");
+        System.out.println("Test Java");
     }
 }
 
